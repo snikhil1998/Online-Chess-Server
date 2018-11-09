44b0fab07687fc3d45c7246841c71f7fc29fa9c1
snikhil1998 <nikhil@localhost.localdomain>
2018-11-09 22:12:45 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+app.use(express.static('public')); 
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/game.html');
+});
+
+http.listen(port, function() {
+    console.log('listening on *: ' + port);
+});
+
+io.on('connection', function(socket) {
+    console.log('new connection');
+
+    // Called when the client calls socket.emit('move')
+    socket.on('move', function(msg) {
+       socket.broadcast.emit('move', msg); 
+    });
+});
---
+++ public/game.js
@@ -0,0 +1,31 @@
+var board;
+var game;
+
+var socket = io();
+
+window.onclick = function(e) {
+  socket.emit('message', 'hello world!');
+};
+
+window.onload = function () {
+    initGame();
+};
+
+var initGame = function() {
+    var cfg = {
+        draggable: true,
+        position: 'start',
+        onDrop: handleMove,
+    };
+   
+    board = new ChessBoard('branding', cfg);
+    game = new Chess();
+};
+
+var handleMove = function(source, target) {
+    var move = game.move({from: source, to: target});
+    
+    if (move === null)  return 'snapback';
+    else socket.emit('move', move);
+    
+};
